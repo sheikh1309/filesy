e28078a5c6108319ece65213848def65f16f449d
Hassan <hassan@moonshot.co.il>
2021-08-19 21:49:21 +0300

    add footer with total to view

--- view/table.go
+++ view/table.go
@@ -3,13 +3,13 @@ package view
 import (
 	"github.com/olekukonko/tablewriter"
 	"os"
+	"strconv"
 )
 
 func Table(headers []string, rows [][]string)  {
 	table := tablewriter.NewWriter(os.Stdout)
 	table.SetHeader(headers)
-	for _, row := range rows {
-		table.Append(row)
-	}
+	table.SetFooter([]string{"", "", "", "Total", strconv.Itoa(len(rows))})
+	table.AppendBulk(rows)
 	table.Render()
 }
